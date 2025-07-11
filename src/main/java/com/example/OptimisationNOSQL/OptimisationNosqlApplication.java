3c558e95f960c56300dfce021ce5d0468a2c0e29
LePariisien <alexandrecano@hotmail.fr>
2025-07-11 09:50:32 +0200

    initialise spring boot project

---
+++ src/main/java/com/example/OptimisationNOSQL/OptimisationNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.OptimisationNOSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisationNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimisationNosqlApplication.class, args);
+	}
+
+}
